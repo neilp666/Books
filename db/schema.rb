84190c590dc554c4cb226f0e66d120872632e34a
neil patel <neil@myindianweddingapp.com>
2015-01-15 19:07:42 +0000

    Added Book User Association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114111624) do
+ActiveRecord::Schema.define(version: 20150115185342) do
 
   create_table "books", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150114111624) do
     t.boolean  "availability", default: true
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
